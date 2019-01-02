19be32f5fa3bad5c3ecdf4a3d6b1018f08ad2c0a
Bruno Farias <brunofarias1@gmail.com>
2019-01-02 10:49:40 +0200

    changed classes to extend PureComponent to avoid update lifecycle verification

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit'
 
-class App extends Component {
+class App extends PureComponent {
   constructor(props) {
     super(props)
     console.log('[App.js] Constructor')
@@ -26,10 +26,10 @@ class App extends Component {
     console.log('[App.js] Inside componentDidMount()')
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('[UPDATE App.js] Inside shoulComponentUpdate()', nextProps, nextState)
-    return nextState.persons !== this.state.persons || nextState.showPersons !== this.state.showPersons
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('[UPDATE App.js] Inside shoulComponentUpdate()', nextProps, nextState)
+  //   return nextState.persons !== this.state.persons || nextState.showPersons !== this.state.showPersons
+  // }
 
   componentWillUpdate(nextProps, nextState) {
     console.log('[UPDATE App.js] Inside componentWillUpdate()', nextProps, nextState)
