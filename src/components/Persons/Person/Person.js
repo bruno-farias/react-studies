339f98402cb42a9a8a752d6a7a93897d6986d2d4
Bruno Farias <brunofarias1@gmail.com>
2019-01-02 11:55:42 +0200

    changed withClass component to use unknow props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
-import WithClasses from '../../../hoc/WithClass'
+import Aux from '../../../hoc/Aux'
+import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
   constructor(props) {
@@ -18,12 +19,12 @@ class Person extends Component {
 
   render() {
     console.log('[Person.js] Inside render()')
-    return <WithClasses classes={classes.Person}>
+    return <Aux>
       <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
       <p>{this.props.children}</p>
       <input type="text" onChange={this.props.changed} value={this.props.name} />
-    </WithClasses>
+    </Aux>
   }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
