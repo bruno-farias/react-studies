318dfcc7933f28e03d9ca25e8c6fba316a6f3820
Bruno Farias <brunofarias1@gmail.com>
2019-01-02 13:08:51 +0200

    added validation of props with PropTypes package

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 import classes from './Person.css';
 import Aux from '../../../hoc/Aux'
 import withClass from '../../../hoc/withClass';
@@ -25,6 +26,13 @@ class Person extends Component {
       <input type="text" onChange={this.props.changed} value={this.props.name} />
     </Aux>
   }
+}
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
 }
 
 export default withClass(Person, classes.Person);
